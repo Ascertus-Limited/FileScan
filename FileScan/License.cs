fcc4263960d0cfad0e195ca29926f3b9669aeba8
asmallwood-ascertus <151163954+asmallwood-ascertus@users.noreply.github.com>
2024-11-29 10:35:38 +0000

    Setting up UI

---
+++ FileScan/License.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using FileScan.Encryption;
+using System.Windows.Forms;
+namespace FileScan
+{
+    internal class License
+    {
+        public bool IsLicensed()
+        {
+            try
+            {
+                string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+                string encryptedString = File.ReadAllText(Path.Combine(appPath, "License.lic")).ToString();
+
+                string decryptedString = Cryptography.Decrypt(encryptedString);
+
+                string[] licenseDetails = decryptedString.Split('|');
+
+                string product = licenseDetails[0];
+                string expiryDate = licenseDetails[1];
+
+                if (DateTime.Now < Convert.ToDateTime(expiryDate) && product == "File Scan")
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
